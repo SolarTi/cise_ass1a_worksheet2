fa88eddb78249a1101ba559d66e0f4048f0cd1ec
SolaireTi <79572077+SolarTi@users.noreply.github.com>
2022-03-18 12:46:50 +1300

    2 Tests Commit

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+/*
+ * @Author: your name
+ * @Date: 2022-03-18 03:59:32
+ * @LastEditTime: 2022-03-18 12:40:25
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \CISE_Repos\cise_ass1a_worksheet2\frontend\src\App.test.js
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
