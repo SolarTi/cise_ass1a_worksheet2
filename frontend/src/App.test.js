4639a17c476cfe3f482d8156fcb928154fc58fc3
SolaireTi <79572077+SolarTi@users.noreply.github.com>
2022-04-04 16:27:28 +1200

    try to fix

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,24 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-03-18 03:59:32
- * @LastEditTime: 2022-03-18 12:48:44
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-04-04 16:26:48
+ * @LastEditors: your name
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \CISE_Repos\cise_ass1a_worksheet2\frontend\src\App.test.js
+ * @FilePath: \cise_ass1a_worksheet2\frontend\src\App.test.js
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
-  });
-});
-
